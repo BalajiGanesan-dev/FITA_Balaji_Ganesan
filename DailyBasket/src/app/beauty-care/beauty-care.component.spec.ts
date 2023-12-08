ea60f2ab9c54ce55dee1ec35702b3547a1b218b9
Chandrikamohan <93970035+Chandrikamohan@users.noreply.github.com>
2023-12-08 11:06:29 +0530

    Chandrika have created products

---
+++ DailyBasket/src/app/beauty-care/beauty-care.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BeautyCareComponent } from './beauty-care.component';
+
+describe('BeautyCareComponent', () => {
+  let component: BeautyCareComponent;
+  let fixture: ComponentFixture<BeautyCareComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BeautyCareComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BeautyCareComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
